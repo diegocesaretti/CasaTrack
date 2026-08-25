60c197cb7799fec9e1eb2c2cb5e8205dfe824d61
diegocesaretti <47791066+diegocesaretti@users.noreply.github.com>
2026-08-25 11:34:08 -0300

    Make transfer invites safely retryable for the same target

--- cloudflare/migrations/0003_transfer_nonce.sql
+++ cloudflare/migrations/0003_transfer_nonce.sql
@@ -1,3 +1,5 @@
 ALTER TABLE enrollment_invites ADD COLUMN used_nonce TEXT;
+ALTER TABLE enrollment_invites ADD COLUMN used_purpose TEXT;
 
 CREATE INDEX IF NOT EXISTS idx_invites_used_nonce ON enrollment_invites(used_nonce);
+CREATE INDEX IF NOT EXISTS idx_invites_used_purpose ON enrollment_invites(used_purpose, expires_at_ms);
